26b8e513b3c808f0381d251b8e4c3504fdbd5f76
Gulam Haqqani <gulamhaqqani@gmail.com>
2020-04-12 15:11:21 -0230

    updated js

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var deleteBtns = document.getElementByClassName("Delete");
-var items = ul.getElementByTagName("li");
+var deleteBtns = document.getElementsByClassName("Delete");
+var items = ul.getElementsByClassNameByTagName("li");
+
+
+for(var i = 0; i < deleteBtns.length; i++){
+	deleteBtns[i].addEventListener("click", removeParent, false);
+}
+
+function removeParent(evt){
+	evt.target.removeEventListener("click", removeParent, false);
+	evt.target.parentNode.remove();
+
+}
+
+function getEventTarget(e){
+	e = e || window.event;
+	return e.target || e.scrElement;
+}
+
+ul.onclick = function(event){
+	var target =getEventTarget(event);
+	target.classList.toggle("done");
+}
 
 function inputLength() {
 	return input.value.length;
